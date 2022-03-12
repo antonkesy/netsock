56b5284b9f2356c26dd9f7b697c93077603b8127
Anton Kesy <anton@kesy.de>
2022-03-12 23:53:45 +0100

    generalize test socket

--- test/util/test_socket.c
+++ test/util/test_socket.c
@@ -7,32 +7,67 @@
 
 #define BUF_SIZE 1024
 
-void test_udp_server(struct sockaddr_in *self, const char *expect_file_path) {
+typedef ssize_t (*recv_fun)(int socket_fd, void *buffer, size_t n);
+
+typedef void(*prep_sock)();
+
+ssize_t recv_udp(int socket_fd, void *buffer, size_t n) {
+    socklen_t len = sizeof(struct sockaddr);
+    return recvfrom(socket_fd, buffer, n, 0, NULL, &len);
+}
+
+ssize_t recv_tcp(int socket_fd, void *buffer, size_t n) {
+    return recv(socket_fd, buffer, n, 0);
+}
+
+void prep_tcp(int socket_fd) {
+    struct sockaddr out;
+    assert(listen(socket_fd, 1) != -1);
+    socklen_t len = sizeof(struct sockaddr_in);
+    assert(accept(socket_fd, &out, &len) != -1);
+}
+
+void test_file_socket(struct sockaddr_in *self, const char *expect_file_path, int type, prep_sock prep, recv_fun recv) {
     int expect_fd = open(expect_file_path, O_RDONLY);
     assert(expect_fd >= 0);
 
-    int socket_fd = socket(self->sin_family, SOCK_DGRAM, 0);
+    int socket_fd = socket(self->sin_family, type, 0);
     assert(socket_fd != -1);
-
-    char *buffer[BUF_SIZE];
-    char *file_buf[BUF_SIZE];
 
     int bind_ret = bind(socket_fd, (const struct sockaddr *) self, sizeof(struct sockaddr));
     assert(bind_ret == 0);
 
+    if (prep != NULL) {
+        prep();
+    }
+
+    char recv_buf[BUF_SIZE];
+    char file_buf[BUF_SIZE];
+
+
     size_t bytes_read;
     size_t bytes_recv;
 
-    socklen_t len = sizeof(struct sockaddr);
 
     do {
-        bytes_recv = recvfrom(socket_fd, buffer, BUF_SIZE, 0, NULL, &len);
+        //TODO not correct when not same read/recv size
+        //TODO add file pos pointers
+        //TODO work with stream
+        bytes_recv = recv(socket_fd, recv_buf, BUF_SIZE);
         bytes_read = read(expect_fd, file_buf, BUF_SIZE);
         assert(bytes_recv >= 0);
         assert(bytes_read == bytes_recv);
-        assert(memcmp(buffer, file_buf, bytes_recv) == 0);
+        assert(memcmp(recv_buf, file_buf, bytes_read) == 0);
     } while (bytes_read > 0);
 
     close(expect_fd);
     close(socket_fd);
+}
+
+void test_udp_server(struct sockaddr_in *self, const char *expect_file_path) {
+    test_file_socket(self, expect_file_path, SOCK_DGRAM, NULL, recv_udp);
+}
+
+void test_tcp_server(struct sockaddr_in *self, const char *expect_file_path) {
+    test_file_socket(self, expect_file_path, SOCK_STREAM, prep_tcp, recv_tcp);
 }
--- test/util/test_socket.h
+++ test/util/test_socket.h
@@ -7,4 +7,6 @@
 
 void test_udp_server(struct sockaddr_in *self, const char *expect_file_path);
 
+void test_tcp_server(struct sockaddr_in *self, const char *expect_file_path);
+
 #endif //NETCOPY_TEST_SOCKET_H
